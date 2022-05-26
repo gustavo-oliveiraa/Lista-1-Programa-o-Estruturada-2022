912972437c2b50e5a624d0601ec6cfc6ebaf318a
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2022-05-25 22:44:46 -0300

    Add files via upload

--- Entrada e saida de dados lista 1.c
+++ Entrada e saida de dados lista 1.c
@@ -5,120 +5,94 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	
-	char Rs[100], Nome[100], Ender[100], Comp[100], Bairro[100], Est[10], Cidade[30], op, Email[30], opEmail[5], placa1[10], placa2[10], placa3[10],  modelo1[20], modelo2[20], modelo3[20], AssResp[30], cor1[15], cor2[15], cor3[15]; 
-	int Num, Cep, DDD1, Tel1, DDD2, Tel2, CPF, CNPJ, InscE, InscM, CNAE, DataN, ContResp, TelCom, Data, NumVeic, ano1, ano2, ano3;
-	
-	
+	char Rs[100], Nome[100], Ender[100], Comp[100], Bairro[100], Est[10], City[30], op, Email[30], opEmail[5], placa1[10], placa2[10], placa3[10],  modelo1[20], modelo2[20], modelo3[20], AssResp[30], cor1[15], cor2[15], cor3[15]; 
+	char Cep[20], Tel1[20], Tel2[20], CPF[20], CNPJ[20], InscE[25], InscM[25], CNAE[25], DataN[20], ContResp[20], TelCom[20], DataH[20];
+	int Num, DDD1, DDD2, NumVeic = 0, ano1, ano2 = 0, ano3 = 0;
+		
 	printf("Digite a Razão Social:");
 	scanf("%[^\n]s", &Rs);
-	fflush(stdin);
-		
+	fflush(stdin);	
 	printf("Digite o Nome Completo:");
 	scanf("%[^\n]s", &Nome);
-	fflush(stdin);
-	
-		
+	fflush(stdin);		
 	printf("Digite o endereço:");
 	scanf("%[^\n]s", &Ender);
-	fflush(stdin);
-	
-		
+	fflush(stdin);		
 	printf("Digite o Número:");
 	scanf("%d", &Num);
-	fflush(stdin);
-	
-		
+	fflush(stdin);		
 	printf("Digite o Complemento:");
 	scanf("%[^\n]s", &Comp);
-	fflush(stdin);
-	
-		
+	fflush(stdin);	
 	printf("Digite o Bairro:");
 	scanf("%[^\n]s", &Bairro);
-	fflush(stdin);
-	
+	fflush(stdin);	
 	printf("Digite o CEP:");
-	scanf("%d", &Cep);
-	fflush(stdin);
-	
+	scanf("%s", &Cep);
+	fflush(stdin);	
 	printf("Digite o Estado:");
 	scanf("%[^\n]s", &Est);
-	fflush(stdin);
-	
+	fflush(stdin);	
 	printf("Digite a Cidade:");
-	scanf("%[^\n]s", &Cidade);
-	fflush(stdin);
-	
+	scanf("%[^\n]s", &City);
+	fflush(stdin);	
 	printf("Digite o DDD, do telefone residencial ou comercial:");
 	scanf("%d", &DDD1);
-	fflush(stdin);
-	
+	fflush(stdin);	
 	printf("Digite o Telefone Residencial ou Comercial:");
-	scanf("%d", &Tel1);
-	fflush(stdin);
-	
+	scanf("%s", &Tel1);
+	fflush(stdin);	
 	printf("Digite o DDD, do telefone celular:");
 	scanf("%d", &DDD2);
-	fflush(stdin);
-	
+	fflush(stdin);	
 	printf("Digite o Telefone Celular:");
-	scanf("%d", &Tel2);
-	fflush(stdin);
-	
+	scanf("%s", &Tel2);
+	fflush(stdin);	
 	printf("Digite seu CPF:");
-	scanf("%d", &CPF);
+	scanf("%s", &CPF);
 	fflush(stdin);
-	
-	
-	
+		
 	printf("Você é uma pessoa Jurídica? Digite (s) - sim ou (n) - não:");
-	scanf("%[^\n]s", &op);
+	scanf("%s", &op);
 	fflush(stdin);
 	
 	switch(op){
 		case 's':
 			printf("Digite seu CNPJ:");
-			scanf("%d", &CNPJ);
+			scanf("%[^\n]s", &CNPJ);
 			fflush(stdin);
 			printf("Digite seu Insc. Estadual:");
-			scanf("%d", &InscE);
+			scanf("%[^\n]s", &InscE);
 			fflush(stdin);
 			printf("Digite seu Insc. Municipal:");
-			scanf("%d", &InscM);
+			scanf("%[^\n]s", &InscM);
 			fflush(stdin);
 			printf("Digite seu CNAE:");
-			scanf("%d", &CNAE);
-			fflush(stdin);
-			
+			scanf("%[^\n]s", &CNAE);
+			fflush(stdin);			
 			printf("Digite sua Data de Nascimento:");
-			scanf("%d", &DataN);
-			fflush(stdin);
-			
+			scanf("%s", &DataN);
+			fflush(stdin);			
 			printf("Digite seu E-mail:");
-			scanf("%[^\n]s", &Email);
-			fflush(stdin);
-			
-			printf("Deseja receber os próximos boletos por E-mail? (s) - sim ou (n) - não");
-			scanf("%[^\n]s", &opEmail);
-			fflush(stdin);
-			
+			scanf("%s", &Email);
+			fflush(stdin);			
+			printf("Deseja receber os próximos boletos por E-mail? (s) - sim ou (n) - não:");
+			scanf("%s", &opEmail);
+			fflush(stdin);			
 			printf("Digite o Contato do Responsável:");
-			scanf("%d", &ContResp);
-			fflush(stdin);
-			
+			scanf("%s", &ContResp);
+			fflush(stdin);			
 			printf("Digite o Telefone Comercial:");
-			scanf("%d", &TelCom);
-			fflush(stdin);
-			
+			scanf("%s", &TelCom);
+			fflush(stdin);			
 			printf("Digite a Data de hoje:");
-			scanf("%d", &Data);
-			fflush(stdin);
-			
+			scanf("%s", &DataH);
+			fflush(stdin);			
 			printf("Assinatura do Responsável:");
 			scanf("%[^\n]s", &AssResp);
 			fflush(stdin);			
 			
-			printf("Quantos veículos possui? min - (0) e max - (3):");
+			printf("Quantos veículos gostaria de registrar min - (1) e max - (3):");
 			scanf("%d", &NumVeic);
 			fflush(stdin);
 			
@@ -126,19 +100,16 @@ int main(){
 				
 				case 1: 
 				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa1);
+				printf("Digite a placa do veículo 1:");
+				scanf("%s", &placa1);
 				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+				printf("Digite o modelo do veículo 1:");
 				scanf("%[^\n]s", &modelo1);
-				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
-				scanf("%[^\n]s", &ano1);
-				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				fflush(stdin);				
+				printf("Digite o ano do veículo 1:");
+				scanf("%d", &ano1);
+				fflush(stdin);				
+				printf("Digite a cor do veículo 1:");
 				scanf("%[^\n]s", &cor1);
 				fflush(stdin);
 				
@@ -146,35 +117,29 @@ int main(){
 				
 				case 2: 
 				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa1);
+				printf("Digite a placa do veículo 1:");
+				scanf("%s", &placa1);
 				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+				printf("Digite o modelo do veículo 1:");
 				scanf("%[^\n]s", &modelo1);
-				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
-				scanf("%[^\n]s", &ano1);
-				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				fflush(stdin);				
+				printf("Digite o ano do veículo 1:");
+				scanf("%d", &ano1);
+				fflush(stdin);				
+				printf("Digite a cor do veículo 1:");
 				scanf("%[^\n]s", &cor1);
 				fflush(stdin);
-				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa2);
-				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+							
+				printf("Digite a placa do veículo 2:");
+				scanf("%s", &placa2);
+				fflush(stdin);				
+				printf("Digite o modelo do veículo 2:");
 				scanf("%[^\n]s", &modelo2);
 				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
-				scanf("%[^\n]s", &ano2);
-				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				printf("Digite o ano do veículo 2:");
+				scanf("%d", &ano2);
+				fflush(stdin);	
+				printf("Digite a cor do veículo 2:");
 				scanf("%[^\n]s", &cor2);
 				fflush(stdin);
 				
@@ -182,51 +147,42 @@ int main(){
 				
 				case 3:
 				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa1);
+				printf("Digite a placa do veículo 1:");
+				scanf("%s", &placa1);
 				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+				printf("Digite o modelo do veículo 1:");
 				scanf("%[^\n]s", &modelo1);
-				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
-				scanf("%[^\n]s", &ano1);
-				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				fflush(stdin);				
+				printf("Digite o ano do veículo 1:");
+				scanf("%d", &ano1);
+				fflush(stdin);				
+				printf("Digite a cor do veículo 1:");
 				scanf("%[^\n]s", &cor1);
 				fflush(stdin);
-				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa2);
-				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+								
+				printf("Digite a placa do veículo 2:");
+				scanf("%s", &placa2);
+				fflush(stdin);				
+				printf("Digite o modelo do veículo 2:");
 				scanf("%[^\n]s", &modelo2);
-				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
-				scanf("%[^\n]s", &ano2);
-				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				fflush(stdin);				
+				printf("Digite o ano do veículo 2:");
+				scanf("%d", &ano2);
+				fflush(stdin);				
+				printf("Digite a cor do veículo 2:");
 				scanf("%[^\n]s", &cor2);
 				fflush(stdin);
-				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa3);
-				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+								
+				printf("Digite a placa do veículo 3:");
+				scanf("%s", &placa3);
+				fflush(stdin);				
+				printf("Digite o modelo do veículo 3:");
 				scanf("%[^\n]s", &modelo3);
-				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
-				scanf("%[^\n]s", &ano3);
-				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				fflush(stdin);				
+				printf("Digite o ano do veículo 3:");
+				scanf("%d", &ano3);
+				fflush(stdin);				
+				printf("Digite a cor do veículo 3:");
 				scanf("%[^\n]s", &cor3);
 				fflush(stdin);
 				
@@ -239,37 +195,29 @@ int main(){
 			}
 			
 		break;
-			
-		
+					
 		case 'n':
 		
 			printf("Digite sua Data de Nascimento:");
-			scanf("%d", &DataN);
-			fflush(stdin);
-			
+			scanf("%s", &DataN);
+			fflush(stdin);			
 			printf("Digite seu E-mail:");
-			scanf("%[^\n]s", &Email);
-			fflush(stdin);
-			
+			scanf("%s", &Email);
+			fflush(stdin);			
 			printf("Deseja receber os próximos boletos por E-mail? (s) - sim ou (n) - não:");
-			scanf("%[^\n]s", &opEmail);
-			fflush(stdin);
-			
+			scanf("%s", &opEmail);
+			fflush(stdin);			
 			printf("Digite o Telefone Comercial:");
-			scanf("%d", &TelCom);
-			fflush(stdin);
-			
+			scanf("%s", &TelCom);
+			fflush(stdin);			
 			printf("Digte a Data de hoje:");
-			scanf("%d", &Data);
-			fflush(stdin);
-			
+			scanf("%s", &DataH);
+			fflush(stdin);			
 			printf("Assinatura do Responsável:");
 			scanf("%[^\n]s", &AssResp);
 			fflush(stdin);
 			
-			
-			
-			printf("Quantos veículos possui? min - (0) e max - (3):");
+			printf("Quantos veículos gostaria de registrar min - (1) e max - (3):");
 			scanf("%d", &NumVeic);
 			fflush(stdin);
 			
@@ -277,19 +225,16 @@ int main(){
 	
 				case 1: 
 				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa1);
+				printf("Digite a placa do veículo 1:");
+				scanf("%s", &placa1);
 				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+				printf("Digite o modelo do veículo 1:");
 				scanf("%[^\n]s", &modelo1);
-				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
+				fflush(stdin);				
+				printf("Digite o ano do veículo 1:");
 				scanf("%d", &ano1);
-				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				fflush(stdin);				
+				printf("Digite a cor do veículo 1:");
 				scanf("%[^\n]s", &cor1);
 				fflush(stdin);
 				
@@ -297,35 +242,29 @@ int main(){
 				
 				case 2: 
 				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa1);
-				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+				printf("Digite a placa do veículo 1:");
+				scanf("%s", &placa1);
+				fflush(stdin);				
+				printf("Digite o modelo do veículo 1:");
 				scanf("%[^\n]s", &modelo1);
-				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
+				fflush(stdin);				
+				printf("Digite o ano do veículo 1:");
 				scanf("%d", &ano1);
 				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				printf("Digite a cor do veículo 1:");
 				scanf("%[^\n]s", &cor1);
 				fflush(stdin);
 				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa2);
-				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+				printf("Digite a placa do veículo 2:");
+				scanf("%s", &placa2);
+				fflush(stdin);				
+				printf("Digite o modelo do veículo 2:");
 				scanf("%[^\n]s", &modelo2);
-				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
+				fflush(stdin);				
+				printf("Digite o ano do veículo 2:");
 				scanf("%d", &ano2);
-				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				fflush(stdin);				
+				printf("Digite a cor do veículo 2:");
 				scanf("%[^\n]s", &cor2);
 				fflush(stdin);
 				
@@ -333,51 +272,42 @@ int main(){
 				
 				case 3:
 				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa1);
-				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+				printf("Digite a placa do veículo 1:");
+				scanf("%%s", &placa1);
+				fflush(stdin);				
+				printf("Digite o modelo do veículo 1:");
 				scanf("%[^\n]s", &modelo1);
-				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
+				fflush(stdin);				
+				printf("Digite o ano do veículo 1:");
 				scanf("%d", &ano1);
-				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				fflush(stdin);				
+				printf("Digite a cor do veículo 1:");
 				scanf("%[^\n]s", &cor1);
 				fflush(stdin);
 				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa2);
-				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+				printf("Digite a placa do veículo 2:");
+				scanf("%s", &placa2);
+				fflush(stdin);				
+				printf("Digite o modelo do veículo 2:");
 				scanf("%[^\n]s", &modelo2);
-				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
+				fflush(stdin);				
+				printf("Digite o ano do veículo 2:");
 				scanf("%d", &ano2);
-				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				fflush(stdin);				
+				printf("Digite a cor do veículo 2:");
 				scanf("%[^\n]s", &cor2);
 				fflush(stdin);
-				
-				printf("Digite a placa do veículo:");
-				scanf("%[^\n]s", &placa3);
-				fflush(stdin);
-				
-				printf("Digite o modelo do veículo:");
+								
+				printf("Digite a placa do veículo 3:");
+				scanf("%s", &placa3);
+				fflush(stdin);				
+				printf("Digite o modelo do veículo 3:");
 				scanf("%[^\n]s", &modelo3);
-				fflush(stdin);
-				
-				printf("Digite o ano do veículo:");
+				fflush(stdin);				
+				printf("Digite o ano do veículo 3:");
 				scanf("%d", &ano3);
-				fflush(stdin);
-				
-				printf("Digite a cor do veículo:");
+				fflush(stdin);				
+				printf("Digite a cor do veículo 3:");
 				scanf("%[^\n]s", &cor3);
 				fflush(stdin);
 				
@@ -398,6 +328,8 @@ int main(){
 			}
 			
 	fflush(stdin);
+	
+	system("cls");
 			
 	printf("Razão Social e Nome Completo:\n");
 	printf("%s\n", Rs);
@@ -406,40 +338,51 @@ int main(){
 	printf("Endereço:\n");
 	printf("%s\n", Ender);
 	
-	printf("Número: \t Complemento: \t \t \t  Bairro:\n");
-	printf("%d \t\t %s \t \t %s\n", Num, Comp, Bairro);
+	printf("Número: \t Complemento: \t \t \t \t  Bairro:\n");
+	printf("%d \t\t %s \t\t\t %s\n", Num, Comp, Bairro);
 	
-	printf("CEP: \t Estado: \t Cidade: \n");
-	printf("%d \t %s \t %s\n", Cep, Est, Cidade);
+	printf("CEP: \t \t \t Estado: \t \t \t Cidade: \n");
+	printf("%s \t \t  %s \t \t \t %s\n", Cep, Est, City);
 	
 	printf("DDD \t Telefone Residencial ou Comercial: \t DDD \t Telefone Celular:\n");
-	printf("%d \t %d \t \t \t %d \t \t \t %d\n", DDD1, Tel1, DDD2, Tel2);
+	printf("%d \t \t %s \t \t \t  %d \t %s\n", DDD1, Tel1, DDD2, Tel2);
 	
-	printf("CPF: \t \t \t \t CNPJ:\n");
-	printf("%d \t \t \t \t %d\n", CPF, CNPJ);
+	printf("CPF: \t \t \t \t \t CNPJ:\n");
+	printf("%s \t \t \t \t %s\n", CPF, CNPJ);
 	
 	printf("Insc. Estadual: \t \t \t \t Insc. Municipal:\n");
-	printf("%d \t \t \t \t %d\n", InscE, InscM);
+	printf("%s \t \t \t \t \t \t %s\n", InscE, InscM);
 	
-	printf("CNAE: \t \t \t Data de Nasc.: \t \t \t Deseja receber os próximos boleto por e-mail?\n")/
-	printf("%d \t \t \t %d \t \t \t %s\n", CNAE, DataN, opEmail);
+	printf("CNAE: \t \t \t Data de Nasc: \t \t \t Deseja receber os próximos boleto por e-mail?\n")/
+	printf("%s \t \t \t %s \t \t \t \t\t  %s\n", CNAE, DataN, opEmail);
 	
 	printf("E-mail:\n");
-	printf("%s", Email);
+	printf("%s\n", Email);
 	
 	printf("Contato do Responsável (em caso de Pessoa Jurídica) \t \t \t Telefone Comercial:\n");
-	printf("%d \t \t \t \t %d \n", ContResp, TelCom);
+	printf("%s \t \t \t \t\t\t\t\t\t %s \n", ContResp, TelCom);
 	
-	printf("Data: \t \t \t \t %s\nAssinatura do Responsável", AssResp);
-	printf("%d\n", Data);
+	printf("Data: \t \t \t \t \t \t \t \t \t %s\n", AssResp);
+	printf("%s \t \t \t \t \t \t \t Assinatura do Responsável\n\n", DataH);
 	
+	printf("DADOS DOS VEÍCULOS\n");
 	
-	
-	
-	
-	
+	if(NumVeic == 1){
 		
+		printf("Placa: \t \t \t Modelo: \t \t \t Ano: \t \t \t \t Cor: \n");
+		printf("%s \t \t \t %s \t \t \t \t %d \t \t \t \t %s\n", placa1, modelo1, ano1, cor1);
+	}else if(NumVeic == 2){
+		
+		printf("Placa: \t \t \t Modelo: \t \t \t Ano: \t \t \t \t Cor: \n");
+		printf("%s \t \t \t %s \t \t \t \t %d \t \t \t \t %s\n", placa1, modelo1, ano1, cor1);
+		printf("%s \t \t \t %s \t \t \t \t %d \t \t \t \t %s\n", placa2, modelo2, ano2, cor2);
+	}else if(NumVeic == 3){
 	
-	
+		printf("Placa: \t \t \t Modelo: \t \t \t Ano: \t \t \t \t Cor: \n");
+		printf("%s \t \t \t %s \t \t \t \t %d \t \t \t \t %s\n", placa1, modelo1, ano1, cor1);
+		printf("%s \t \t \t %s \t \t \t \t %d \t \t \t \t %s\n", placa2, modelo2, ano2, cor2);
+		printf("%s \t \t \t %s \t \t \t \t %d \t \t \t \t %s\n", placa3, modelo3, ano3, cor3);
+	}
+			
 	return 0;
 }
